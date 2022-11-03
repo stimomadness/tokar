e93d26adbc2b0c923acbc75c0f3153aab665bb87
StimoMadness <stimomadness@gmail.com>
2022-11-03 10:47:45 +0300

    added card-one page and slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,50 @@
-$(function () {
+// 'use strict'
 
+
+// function filterFunction() {
+//   var input, filter, a, i;
+//   input = document.getElementById("myInput");
+//   filter = input.value.toUpperCase();
+//   div = document.getElementById("myDropdown");
+//   a = div.getElementsByTagName("a");
+//   for (i = 0; i < a.length; i++) {
+//     if (a[i].innerHTML.toUpperCase().indexOf(filter) > -1) {
+//       a[i].style.display = "";
+//     } else {
+//       a[i].style.display = "none";
+//     }
+//   };
+// };
+
+$(document).ready(function () {
+  var list = $(".shop-catalogy .shop-catalogy__item");
+  var numToShow = 9;
+  var button = $(".shopBtn");
+  var numInList = list.length;
+  list.hide();
+  if (numInList > numToShow) {
+    button.show();
+  }
+  list.slice(0, numToShow).show();
+  button.click(function () {
+    var showing = list.filter(':visible').length;
+    list.slice(showing - 1, showing + numToShow).fadeIn();
+    var nowShowing = list.filter(':visible').length;
+    if (nowShowing >= numInList) {
+      button.hide();
+    }
+  });
 });
+
+
+$('.select-style, .product-one__item-num').styler();
+
+$('.filter-price__input').ionRangeSlider({
+});
+$('.filter-metr__input').ionRangeSlider({
+});
+
+
 
 $('.header-inner__slider').slick({
   dots: true,
@@ -31,54 +75,22 @@ $('.ceo__clider').slick({
 
 });
 
-
-$('.select-style, .product-one__item-num').styler();
-
-$('.filter-price__input').ionRangeSlider({
+$('.card-one__big').slick({
+  asNavFor: '.card-one__thumb',
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: true,
+  dots: true,
+  fade: true,
+  prevArrow: '<button type="button" class="slick-prev"><svg width="18" height="12" viewBox="0 0 18 12" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M0.469669 6.53033C0.176777 6.23744 0.176777 5.76256 0.469669 5.46967L5.24264 0.6967C5.53553 0.403807 6.01041 0.403807 6.3033 0.6967C6.59619 0.989594 6.59619 1.46447 6.3033 1.75736L2.06066 6L6.3033 10.2426C6.59619 10.5355 6.59619 11.0104 6.3033 11.3033C6.01041 11.5962 5.53553 11.5962 5.24264 11.3033L0.469669 6.53033ZM18 6.75L1 6.75L1 5.25L18 5.25L18 6.75Z" fill="white"/></svg></button>',
+  nextArrow: '<button type="button" class="slick-next"><svg width="18" height="12" viewBox="0 0 18 12" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M17.5303 6.53033C17.8232 6.23744 17.8232 5.76256 17.5303 5.46967L12.7574 0.6967C12.4645 0.403807 11.9896 0.403807 11.6967 0.6967C11.4038 0.989594 11.4038 1.46447 11.6967 1.75736L15.9393 6L11.6967 10.2426C11.4038 10.5355 11.4038 11.0104 11.6967 11.3033C11.9896 11.5962 12.4645 11.5962 12.7574 11.3033L17.5303 6.53033ZM-6.55671e-08 6.75L17 6.75L17 5.25L6.55671e-08 5.25L-6.55671e-08 6.75Z" fill="white" /></svg></button>',
 });
-$('.filter-metr__input').ionRangeSlider({
+$('.card-one__thumb').slick({
+  asNavFor: '.card-one__big',
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  vertical: true,
+  focusOnSelect: true,
+  arrows: false,
+  dots: false,
 });
-
-function myFunction() {
-  document.getElementById("myDropdown").classList.toggle("active");
-};
-
-function filterFunction() {
-  var input, filter, ul, li, a, i;
-  input = document.getElementById("myInput");
-  filter = input.value.toUpperCase();
-  div = document.getElementById("myDropdown");
-  a = div.getElementsByTagName("a");
-  for (i = 0; i < a.length; i++) {
-    if (a[i].innerHTML.toUpperCase().indexOf(filter) > -1) {
-      a[i].style.display = "";
-    } else {
-      a[i].style.display = "none";
-    }
-  };
-};
-
-$(document).ready(function () {
-  var list = $(".shop-catalogy .shop-catalogy__item");
-  var numToShow = 9;
-  var button = $("button");
-  var numInList = list.length;
-  list.hide();
-  if (numInList > numToShow) {
-    button.show();
-  }
-  list.slice(0, numToShow).show();
-  button.click(function () {
-    var showing = list.filter(':visible').length;
-    list.slice(showing - 1, showing + numToShow).fadeIn();
-    var nowShowing = list.filter(':visible').length;
-    if (nowShowing >= numInList) {
-      button.hide();
-    }
-  });
-});
-// $('.acc__item-one').on('click', function (e) {
-//   e.preventDefault();
-//   $('.acc__item-one').removeClass('acc__item-one--active');
-//   $(this).addClass('acc__item-one--active');
-// });
